36145732c24f4812dd9d2548b4fb6101969cb661
Elvis <edigitalb@gmail.com>
2016-03-06 12:41:30 -0300

    increasing the example

--- groovy-01/t03-functions.groovy
+++ groovy-01/t03-functions.groovy
@@ -1,4 +1,8 @@
 println removeLast('Hello World')
+println removeLast2('Hello World')
 def removeLast(String str) {
+	return str.substring(0, str.length()-1);
+}
+def removeLast2(String str) {
 	str.substring(0, str.length()-1);
 }
